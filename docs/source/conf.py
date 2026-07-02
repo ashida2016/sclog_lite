548df14abbd78a8dfdac85b1614a26a1ad031496
ashida2016 <ashida2016@outlook.com>
2026-07-02 14:43:28 +0800

    Release Ver0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'sclog_lite'
 copyright = '2026, Ashida.Shi'
 author = 'Ashida.Shi'
-release = '0.2.0'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
